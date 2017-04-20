247b9162cd9ad6c2ce52ed19a737806b9f20fea5
apexlife <info@apexlife.ru>
2017-04-21 01:50:57 +0400

    Create some changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
-/**
- * Created by kanta on 18.04.2017.
- */
+function toggleClass(element, className){
+    if (!element || !className){
+        return;
+    }
+
+    var classString = element.className, nameIndex = classString.indexOf(className);
+    if (nameIndex == -1) {
+        classString += ' ' + className;
+    }
+    else {
+        classString = classString.substr(0, nameIndex) + classString.substr(nameIndex+className.length);
+    }
+    element.className = classString;
+}
+
+document.getElementById('toggle').addEventListener('click', function() {
+    toggleClass(document.getElementById('overlay'), 'open');
+});
